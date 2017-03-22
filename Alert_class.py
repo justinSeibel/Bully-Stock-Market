cd912fd3be49e479c1196bcbf7cd659c21c59529
caa188 <caa188@msstate.edu>
2017-03-21 20:13:26 -0500

    Alert class
    alert class python

---
+++ Alert_class.py
@@ -0,0 +1,33 @@
+class Alert:
+    
+    def __init__(self, threshold)
+        self.threshold = threshold
+
+    #When a Company's stock reaches threshold ()  
+    class Company_alert:
+        def __init__(self, obsComp)
+            self.company = obsComp
+
+    #When Stock price reaches threshold (stock price) -> sendAlert 
+    class Stock_alert:
+        def __init__(self, obsStock)
+            self.stock = obsStock
+    
+    #When a company's net profit/loss reaches threshold (profit/loss)
+    class Net_alert:
+        def __init__(self, obsComp)
+            self.company = obsComp
+
+    
+    def SendAlert():
+        
+    def ClearAlert(Alert):
+        del self
+        
+    def getThreshold(Alert):
+        self.threshold = input("Enter the threshold value: ")
+    
+    def setThreshold_Stock():
+    def setThreshold_Company():
+    def setThreshold_Net():
+    
