a61b72d2122fa2242baa6dbb4af7f8411e095daf
evbrower <evan.j.brower@gmail.com>
2017-04-07 20:11:29 -0500

    Login Function added
    Added a login function  (not implemented) with the possibility of
    reading from a csv

--- main.py
+++ main.py
@@ -4,43 +4,67 @@
 
 import yahoo_finance
 from Tkinter import *
+import csv
 
 class MainWin:
 
-    def __init__(self, master):
+	def __init__(self, master):
 
-        self.frame = Frame(master)
-        self.frame.pack()
+		self.frame = Frame(master)
+		self.frame.pack()
+		
+		self.welcome = Label(self.frame, text="Welcome to Bully Stock Market")
+		self.welcome.grid(row=0, columnspan=2)
+		
+		self.userLabel = Label(self.frame, text="Username: ")
+		self.userEntry = Entry(self.frame)
+		self.userLabel.grid(row=1, column=0, sticky=W)
+		self.userEntry.grid(row=1, column=1)
+		
+		self.pwdLabel = Label(self.frame, text="Password: ")
+		self.pwdEntry = Entry(self.frame)
+		self.pwdLabel.grid(row=2, column=0, sticky=W)
+		self.pwdEntry.grid(row=2, column=1)
+		
+		self.registerButton = Button(self.frame, text="Register", command=self.registerSubmit())
+		self.loginButton = Button(self.frame, text="Login", command=self.loginSubmit())
+		self.loginButton.grid(row=3, column=1)
+
         
-        self.welcome = Label(self.frame, text="Welcome to Bully Stock Market")
-        self.welcome.grid(row=0, columnspan=2)
-        
-        self.userLabel = Label(self.frame, text="Username: ")
-        self.userEntry = Entry(self.frame)
-        self.userLabel.grid(row=1, column=0, sticky=W)
-        self.userEntry.grid(row=1, column=1)
-
-        self.pwdLabel = Label(self.frame, text="Password: ")
-        self.pwdEntry = Entry(self.frame)
-        self.pwdLabel.grid(row=2, column=0, sticky=W)
-        self.pwdEntry.grid(row=2, column=1)
-        
-        
-        self.button = Button(self.frame, text="test", command=self.test )
-        self.button.grid(row=3, columnspan=2)
-
-
-    def test(self):
-        lab = Label(self.frame, text="Hello world!")
-        lab.grid(row=4, columnspan=2)
-
+	def loginSubmit(self):
+		#todo
+		#load Entry data to variables
+		self.username = self.userEntry
+		self.password = self.pwdEntry
+		#call login function
+		
+  
+	def registerSubmit(self):
+		#todo
+		self.username = self.userEntry
+		self.password = self.pwdEntry
+		#call register function
+	
+	#WIP
+	def login(self):
+		#search csv file
+		isUser = False
+		usersFile = open('users.csv')
+		users = csv.reader(usersFile)
+		for row in users:
+			if (row == self.username):
+				isUser = True
+				userRow = row
+		correctLogin = False
+		if (row[1] == self.password):
+			correctLogin = True
+		
 
 def main():
-    root = Tk()
-    mainWin = MainWin(root)
-    root.mainloop()
+	root = Tk()
+	mainWin = MainWin(root)
+	root.mainloop()
 
 
 if __name__ == "__main__":
-    main()
-    #example
+	main()
