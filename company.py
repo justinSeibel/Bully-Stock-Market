6459f93f350cfe4c8fac72ef8353f86616297a20
SuperNerdMiles <snmiles253@gmail.com>
2017-04-11 20:19:48 -0500

    Style update
    PyCharm was yelling at me about some style errors I was making, I fixed
    a few of them but I really don't want to change the function names so
    those are staying :P

--- company.py
+++ company.py
@@ -1,17 +1,17 @@
 class Company:
     def __init__(self, name, tag, data, funds):
-        #Would "name" not be given but instead found from the API based on the tag given?
+        # Would "name" not be given but instead found from the API based on the tag given?
         self.companyName = name
         self.companyTag = tag
-        #Would "data" not be given but instead found from the API based on the tag given? Or how in general would this
-        #work?
+        # Would "data" not be given but instead found from the API based on the tag given? Or how in general would this
+        # work?
         self.stockData = data
-        #Would "funds" not be given but instead found from the program based on the user in question? Or how in general
-        #would this work?
+        # Would "funds" not be given but instead found from the program based on the user in question? Or how in general
+        # would this work?
         self.userFundsInvested = funds
 
     def updateData(self):
-        #I forget what this one's supposed to do
+        # I forget what this one's supposed to do
         pass
 
     def getName(self):
