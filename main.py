9d18943dfa427351e5f93b2619a36930740b128d
evbrower <evan.j.brower@gmail.com>
2017-04-12 16:54:44 -0500

    Added StockWin, fixed errors
    Added some imports (that I don't have yet) and added StockWin frame that
    displays a user's stocks.

--- main.py
+++ main.py
@@ -3,28 +3,34 @@
 # reference: http://effbot.org/tkinterbook
 
 import yahoo_finance
-from Tkinter import *
+import Tkinter as tk
 import csv
+import Portfolio
+import Alerts
+import Stock
 
 #ref: http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
-class MainWin(Tk):
+class MainWin(tk.Tk):
 	
 	def __init__(self):
-		Tk.__init__(self)
+		tk.Tk.__init__(self)
 		
-		self.username = self.password = self.portfolio = None
+		self.username = "\0"
+		self.alerts = Alerts()
+		self.portfolio = Portfolio()
 		
 		#pack initial container, then subsequent pages
-		container = Frame(self)
+		container = tk.Frame(self)
 		container.pack(side="top", fill="both", expand=True)
 		container.grid_rowconfigure(0, weight=1)
 		container.grid_columnconfigure(0, weight=1)
 		
 		self.frames = {}
-		for F in (LoginWin):
-			frame = F(container, self)
-			self.frames[F] = frame
-			frame.grid(row=0,column=0,sticky=NSEW)
+		for F in (LoginWin, PortfolioWin, FundTransWin):
+			pageName = F.__name__
+			frame = F(parent=container, controller=self)
+			self.frames[pageName] = frame
+			frame.grid(row=0,column=0,sticky="NSEW")
 		self.showFrame("LoginWin")
 		
 	#reveals the wanted frame
@@ -32,11 +38,9 @@ class MainWin(Tk):
 		frame = self.frames[pageName]
 		frame.tkraise()
 		
-	def storeData(self, username, password, portfolio):
+	def storeData(self, username, alerts, portfolio):
 		self.username = username
-		self.password = password
-		
-		#portfolio = filename? may be redundant
+		self.alerts = alerts
 		self.portfolio = portfolio
 	
 	def getUsername(self):
@@ -48,34 +52,39 @@ class MainWin(Tk):
 		return self.portfolio
 	def setPortfolio(self, portfolio):
 		self.portfolio = portfolio
+	
+	def getAlerts(self):
+		return self.alerts
+	def setAlerts(self, alerts):
+		self.alerts = alerts
 
 
 '''
 Login page
 links to: PortfolioWin
 '''
-class LoginWin(Frame):
+class LoginWin(tk.Frame):
 
 	def __init__(self, parent, controller):
-
-		Frame.__init__(parent)
+		
+		tk.Frame.__init__(self, parent)
 		self.controller = controller
 		
-		self.welcome = Label(self, text="Welcome to Bully Stock Market")
+		self.welcome = tk.Label(self, text="Welcome to Bully Stock Market")
 		self.welcome.grid(row=0, columnspan=2)
 		
-		self.userLabel = Label(self, text="Username: ")
-		self.userEntry = Entry(self)
-		self.userLabel.grid(row=1, column=0, sticky=W)
+		self.userLabel = tk.Label(self, text="Username: ")
+		self.userEntry = tk.Entry(self)
+		self.userLabel.grid(row=1, column=0, sticky=tk.W)
 		self.userEntry.grid(row=1, column=1)
 		
-		self.pwdLabel = Label(self, text="Password: ")
-		self.pwdEntry = Entry(self)
-		self.pwdLabel.grid(row=2, column=0, sticky=W)
+		self.pwdLabel = tk.Label(self, text="Password: ")
+		self.pwdEntry = tk.Entry(self)
+		self.pwdLabel.grid(row=2, column=0, sticky=tk.W)
 		self.pwdEntry.grid(row=2, column=1)
 		
-		self.registerButton = Button(self, text="Register", command=self.registerSubmit())
-		self.loginButton = Button(self, text="Login", command=self.loginSubmit())
+		self.registerButton = tk.Button(self, text="Register", command=self.registerSubmit())
+		self.loginButton = tk.Button(self, text="Login", command=self.loginSubmit())
 		self.loginButton.grid(row=3, column=1)
 		
 	
@@ -99,17 +108,22 @@ class LoginWin(Frame):
 	def login(self):
 		#search csv file
 		isUser = False
-		userRow = None
-		usersFile = open('users.csv')
+		userRow = ['\0', '\0']
+		usersFile = open('users.csv', 'a+')
 		users = csv.reader(usersFile)
-		for row in users:
-			if (row[0] == self.username):
-				isUser = True
-				userRow = row
-				break
-		correctLogin = False
-		if (userRow[1] == self.password):
-			correctLogin = True
+		if users == None:
+			correctLogin = False
+		else:
+			for row in users:
+				if (row[0] == self.username):
+					isUser = True
+					userRow = row
+					break
+			
+			correctLogin = False
+			if isUser & (userRow[1] == self.password):
+				correctLogin = True
+			
 		if ~correctLogin | ~isUser:
 			#placeholder
 			print "Username or password incorrect"
@@ -121,17 +135,22 @@ class LoginWin(Frame):
 	
 	def register(self):
 		userAvail = True
-		usersFile = open('users.csv', 'a')
+		usersFile = open('users.csv', 'a+')
 		users = csv.reader(usersFile)
-		for row in users:
-			if (row[0] == self.username):
-				userAvail = False
+		if users == None:
+			userAvail = True
+			users.write("Username, Password")
+		else:
+			for row in users:
+				if (row[0] == self.username):
+					userAvail = False
 		if ~userAvail:
 			#placeholder
 			print "Username already taken"
 		else:
 			#placeholder
 			#need to write user data to csv file
+			users.write(self.username, self.password)
 			print "Account created successfully"
 			self.controller.showFrame("PortfolioWin")
 
@@ -139,47 +158,52 @@ class LoginWin(Frame):
 
 '''
 Portfolio page
-links to: FundTransWin, SearchWin, Logout(?)
+links to: FundTransWin, SearchWin, StockWin
 '''
-class PortfolioWin(Frame):
+class PortfolioWin(tk.Frame):
 	
 	def __init__(self, parent, controller):
-		Frame.__init__(parent)
+		tk.Frame.__init__(self, parent)
 		self.controller = controller
 		
-		self.welcomeLabel = Label(self, text="Welcome, "+controller.getUsername())
-		self.welcomeLabel.pack(grid=0, columnspan=2)
+		#ref: https://www.tutorialspoint.com/python/tk_panedwindow.htm
+		#self.alertPane = tk.PanedWindow()
 		
-		self.fundLabel = Label(self, text="Current funds: "+controller.portfolio.currentFunds)
-		self.fundLabel.pack(grid=2, column=0)
+		self.welcomeLabel = tk.Label(self, text="Welcome, "+controller.getUsername())
+		self.welcomeLabel.grid(row=0, column=0, columnspan=2)
 		
-		self.addFundButton = Button(self, text="Add/Remove Funds", function=self.controller.showFrame("FundTransWin"))
-		self.addFundButton.pack(grid=2, column=2)
+		self.fundLabel = tk.Label(self, text="Current funds: "+controller.portfolio.getFunds())
+		self.fundLabel.grid(row=2, column=0)
 		
+		self.addFundButton = tk.Button(self, text="Add/Remove Funds", function=self.controller.showFrame("FundTransWin"))
+		self.addFundButton.grid(row=2, column=2)
+		
+		self.seeStockButton = tk.Button(self, text="See Your Stocks", function=self.controller.showFrame("StockWin"))
+		self.seeStockButton.grid(row=3, column=0)
 		
 '''
 Funds transaction page
 links to: PortfolioWin
 '''
-class FundTransWin(Frame):
+class FundTransWin(tk.Frame):
 	
 	def __init__(self, parent, controller):
-		Frame.__init__(parent)
+		tk.Frame.__init__(self, parent)
 		self.controller = controller
 		
-		self.titleLabel = Label(self, text="Add or Remove Funds")
-		self.titleLabel.pack(grid=0, columnspan=2)
+		self.titleLabel = tk.Label(self, text="Add or Remove Funds")
+		self.titleLabel.grid(row=0, columnspan=2)
 		
-		self.valLabel = Label(self, text="Enter value: ")
-		self.valLabel.pack(grid=1, column=0)
+		self.valLabel = tk.Label(self, text="Enter value: ")
+		self.valLabel.grid(row=1, column=0)
 		
-		self.enterVal = Entry(self)
-		self.enterVal.pack(grid=1, column=1)
+		self.enterVal = tk.Entry(self)
+		self.enterVal.grid(row=1, column=1)
 		
-		self.addButton = Button(self, text="Add Funds", function=self.addFundsWin())
-		self.addButton.pack(grid=2, column=0)
-		self.addButton = Button(self, text="Remove Funds", function=self.removeFundsWin())
-		self.addButton.pack(grid=2, column=1)
+		self.addButton = tk.Button(self, text="Add Funds", function=self.addFundsWin())
+		self.addButton.grid(row=2, column=0)
+		self.addButton = tk.Button(self, text="Remove Funds", function=self.removeFundsWin())
+		self.addButton.grid(row=2, column=1)
 	
 	
 	#calls portfolio's addFund function
@@ -188,8 +212,8 @@ class FundTransWin(Frame):
 			self.controller.portfolio.addFunds(self.enterVal.get())
 			self.controller.showFrame("PortfolioWin")
 		else:
-			errorLabel = Label(self, text="No value entered!")
-			errorLabel.pack(grid=3, column=0)
+			errorLabel = tk.Label(self, text="No value entered!")
+			errorLabel.grid(row=3, column=0)
 	
 	
 	#calls portfolio's removeFund function
@@ -198,9 +222,76 @@ class FundTransWin(Frame):
 			self.controller.portfolio.removeFunds(self.enterVal.get())
 			self.controller.showFrame("PortfolioWin")
 		else:
-			errorLabel = Label(self, text="No value entered!")
-			errorLabel.pack(grid=3, column=0)
+			errorLabel = tk.Label(self, text="No value entered!")
+			errorLabel.grid(row=3, column=0)
 
+
+
+'''
+Stock sell page
+links to: PortfolioWin
+'''
+class StockWin(tk.Frame):
+	
+	def __init__(self, parent, controller):
+		
+		tk.Frame.__init__(self, parent)
+		self.controller = controller
+		
+		#used in generating stock table
+		self.portfolio = controller.getPortfolio()
+		
+		self.nameLabel = tk.Label(self, text="Name:")
+		self.nameLabel.grid(row=0, column=1)
+		
+		self.ownedLabel = tk.Label(self, text="Amount Owned:")
+		self.ownedLabel.grid(row=0, column=2)
+		
+		self.currentValLabel = tk.Label(self, text="Current Value:")
+		self.currentValLabel.grid(row=0, column=3)
+		
+		self.netGainLossLabel = tk.Label(self, text="Net Gain/Loss:")
+		self.netGainLossLabel.grid(row=0, column=4)
+		
+		#ref: http://stackoverflow.com/questions/7300041/tkinter-create-labels-and-entrys-dynamically
+		#generate list of stocks
+		portfolioList = self.portfolio.showOwnedStock()
+		self.radioButtonVar = ""
+		x = 1
+		for stock in portfolioList:
+			thisStock = Stock(stock)
+			
+			rb = tk.Radiobutton(self, text="Select", variable=self.radioButtonVar, value=stock)
+			rb.grid(row=x, column=0)
+			
+			#Portfolio class needs function to return amount of stock owned
+			stockAmountLabel = tk.Label(self, text=self.portfolio)
+			stockAmountLabel.grid(row=x, column=1)
+			
+			stockValLabel = tk.Label(self, text=thisStock.getCurrVal())
+			stockValLabel.grid(row=x, column=2)
+			
+			if self.portfolio.showNetGain() > 0:
+				stockNetLabel = tk.Label(self, text=self.portfolio.showNetGain())
+				stockNetLabel.grid(row=x, column=3)
+			elif self.portfolio.showNetLoss() > 0:
+				stockNetLabel = tk.Label(self, text=self.portfolio.showNetLoss())
+				stockNetLabel.grid(row=x, column=3)
+		
+		self.amountLabel = tk.Label(self, text="Amount to Sell: ")
+		self.amountLabel.grid(row=x+1, column=0)
+		
+		self.amountEntry = tk.Entry(self)
+		self.amountEntry.grid(row=x+1, column=1)
+		
+		self.sellButton = tk.Button(self, text="Sell Stock", function=self.sellStock())
+		self.sellButton.grid(row=x+1, column=2)
+		
+		
+	def sellStock(self):
+		self.portfolio.sellStock(self.radioButtonVar, self.amountEntry.get())
+		self.controller.showFrame("PortfolioWin")
+	
 
 
 def main():
