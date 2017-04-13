b1c3e84ee29d443c7b2819b1a1590f84fc58af39
teriannaj <tdj190@msstate.edu>
2017-04-12 20:39:30 -0500

    Add files via upload

--- Stock.py
+++ Stock.py
@@ -1,23 +1,47 @@
-
 """
 Bully Stock Market
 """
 
 
 from yahoo_finance import *
+import urllib
+import urllib2
 
 class Stock():
 
     def __init__(self, stock):
         self.stock = str(stock)
-        self.yahoo = Share(self.stock)
-        self.value = self.yahoo.get_price()
 
 
-    def setValWhenBought(self, price = 0):
-        self.value = self.yahoo.get_price()
-        if(price != 0):
-            self.value = price
+    def search_stock(self):
+        url = urllib2.urlopen('http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=' + self.stock +'&region=1&lang=en&callback=YAHOO.Finance.SymbolSuggest.ssCallback')
+        the_page = url.read()
+        #the_page = the_page.split()
+        length = len(the_page)
+
+        for each in range(0, length):
+            if the_page[each] == '"':
+                if the_page[each + 1] == 's':
+                    index = each + 1
+                    break
+
+        symbol = []
+        for each in range(index + 9, length):
+            if the_page[each] != '"':
+                symbol.append(the_page[each])
+            if the_page[each] == '"':
+                break
+
+        symbol = ''.join(symbol)
+
+        self.symbol = symbol
+        self.yahoo = Share(self.symbol)
+        self.value = self.yahoo.get_price() #sets Value at which the stock is bought
+
+
+    def get_symbol(self):
+        return self.symbol
+
 
     def getCurrVal(self):
         return self.value
@@ -27,3 +51,18 @@ class Stock():
 
     def getValWhenBought(self):
         return self.value
+
+
+
+
+
+
+
+
+
+def main():
+    stock = Stock('intel') #you can just change the name to search for whatever stock
+    stock.search_stock()
+    print 'The symbol of the stock you are searching is: ', stock.get_symbol()
+    print 'The current value of this stock is: ', stock.getCurrVal()
+main()
