35464f2cfe5690dda297733806d2e7181a146fd8
SuperNerdMiles <snmiles253@gmail.com>
2017-04-15 12:51:12 -0500

    Comments and Initializer fix
    Fixed initializer to collect companyName and companyTag from the stock
    passed as "data" (which is set as stockData)
    Also commented functions better

--- company.py
+++ company.py
@@ -1,13 +1,16 @@
+import Stock
+
 class Company:
-    def __init__(self, name, tag, data, funds):
-        # Would "name" not be given but instead found from the API based on the tag given?
-        self.companyName = name
-        self.companyTag = tag
-        # Would "data" not be given but instead found from the API based on the tag given? Or how in general would this
-        # work?
+    def __init__(self, data, funds):
+        # Initializer function. Takes in "data" (a Stock object) for stockData, then uses the object to set companyName
+        # and companyTag. Currently also takes in "funds" (float) for userFundsInvested (should this start as 0 or
+        # otherwise be pulled from the user in question?)
         self.stockData = data
-        # Would "funds" not be given but instead found from the program based on the user in question? Or how in general
-        # would this work?
+
+        self.companyName = self.stockData.getName()
+        self.companyTag = self.stockData.get_symbol()
+
+        # Todo: Figure out how userFundsInvested is actually collected
         self.userFundsInvested = funds
 
     def updateData(self):
@@ -15,29 +18,41 @@ class Company:
         pass
 
     def getName(self):
+        # Getter to return companyName
         return self.companyName
 
     def getTag(self):
+        # Getter to return companyTag
         return self.companyTag
 
     def getStock(self):
+        # Getter to return stockData
         return self.stockData
 
     def getUFI(self):
+        # Getter to return userFundsInvested
         return self.userFundsInvested
 
     def setName(self, name):
+        # Setter to set companyName, returns True if successful
+        # Todo: Error checking to ensure invalid inputs are blocked and setter returns False instead
         self.companyName = name
         return True
 
     def setTag(self, tag):
+        # Setter to return companyTag, returns True if successful
+        # Todo: Error checking to ensure invalid inputs are blocked and setter returns False instead
         self.companyTag = tag
         return True
 
     def setStock(self, data):
+        # Setter to return stockData, returns True if successful
+        # Todo: Error checking to ensure invalid inputs are blocked and setter returns False instead
         self.stockData = data
         return True
 
     def setUFI(self, funds):
+        # Setter to return UserFundsInvested, returns True if successful
+        # Todo: Error checking to ensure invalid inputs are blocked and setter returns False instead
         self.userFundsInvested = funds
         return True
