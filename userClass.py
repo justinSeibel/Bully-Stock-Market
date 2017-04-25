e361c969769fd25f411431e9bd9271cf414fa175
justinSeibel <jstnsbl@gmail.com>
2017-04-25 10:06:50 -0500

    Update userClass.py

--- userClass.py
+++ userClass.py
@@ -1,9 +1,11 @@
 import csv
+from Portfolio import *
 
 class User:
     def __init__(self, userID=" ", password=" "):
         self.ID = userID
         self.password = password
+        #self.portfolio = Portfolio(ID)
 
     def setUserID(self, newID):
         self.ID=newID
@@ -23,14 +25,19 @@ class User:
             s_list.writerow([userid, password])
         return True
 
-    def getUserList(self):
-        info=[]
-        with open('users.csv') as csvfile:
-            fieldnames=['ID']
-            reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-            for row in reader:
-                ID=row['ID']
-                info.append(ID.split(" "))
-        return info
-        
-
+def getUserList():
+    info=[]
+    with open('users.csv') as csvfile:
+        fieldnames=['ID']
+        reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+        users=[]
+        for row in reader:
+            pair=[]
+            ID=row['ID']
+            info=ID.split(" ")
+            name=info[0]
+            password=info[1]
+            pair.append(name)
+            pair.append(password)
+            users.append(pair)
+    return users
